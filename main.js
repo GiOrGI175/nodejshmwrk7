72a7b6065aec7aaeeb5aa88366e7d79a07661ae9
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-12-13 16:22:20 +0400

    feature/filter

--- main.js
+++ main.js
@@ -1,34 +1,55 @@
 import nodefetch from 'node-fetch';
 import { readFile, writeFile } from './utiles/utiles.js';
 import http from 'http';
+import url from 'url';
+import querystring from 'querystring';
 
 async function main() {
   const res = await nodefetch('https://dummyjson.com/users');
 
   const data = await res.json();
 
-  writeFile('users.json', data, true);
+  const users = data.users;
+
+  writeFile('users.json', users, true);
 }
 
 main();
 
 const server = http.createServer(async (req, res) => {
-  const users = await readFile('users.json');
+  const users = await readFile('users.json', true);
 
-  if (req.url === '/users') {
+  // console.log(users);
+
+  const parsedUrl = url.parse(req.url);
+  const queryParams = querystring.parse(parsedUrl.query);
+  // console.log(parsedUrl, 'parsedUrl');
+  // console.log(queryParams, 'queryParams');
+
+  if (parsedUrl.pathname === '/users') {
+    // console.log(queryParams.firstName, 'queryParams.firstName');
+    if (queryParams.firstName) {
+      const filteredUsers = users.filter((item) =>
+        item.firstName.includes(queryParams.firstName)
+      );
+      res.setHeader('Content-type', 'application/json');
+      res.write(JSON.stringify(filteredUsers));
+      res.end();
+      return;
+    }
+
     res.setHeader('Content-type', 'application/json');
     res.write(JSON.stringify(users));
     res.end();
-  } else if (req.url === '/home') {
-    res.setHeader('Content-type', 'application/html');
+    return;
+  }
+  if (req.url === '/home') {
+    res.setHeader('Content-type', 'text/html');
     res.write('<h1>Home Page</h1>');
     res.end();
-  } else {
-    res.statusCode = 404;
-    res.setHeader('Content-type', 'text/plain');
-    res.write('Page not found');
-    res.end();
+    return;
   }
+
   res.write('Hello World');
   res.end('hi');
 });
